8fe76af236c65ab77cfcb0d49e0c966d4d8445db
hoang-mai05 <hoang.mai@tufts.edu>
2025-11-04 14:16:24 -0500

    Integrated Color Detection into Branch Code

--- include/colorDetect.h
+++ include/colorDetect.h
@@ -1,7 +1,6 @@
 /*
  *  colorDetect.h
- *  Hoang Mai
- *  10/16/2025
+ *  11/3/2025
  *
  *
  *  colorDetect.h will define the functionalities of the color detection
@@ -9,6 +8,21 @@
 #ifndef COLOR_DETECT_H
 #define COLOR_DETECT_H
 
+// Color class labels
+enum ColorClass {
+	COLOR_BLACK = 0,
+	COLOR_BLUE  = 1,
+	COLOR_YELLOW= 2,
+	COLOR_RED   = 3
+};
+
+// Initializes the pins and captures ambient baseline from A5
+void colorDetectSetup();
+
+// Performs one measurement cycle and returns ColorClass
+int detectColorClass(int delayMs = 50);
+
+// Calculates the closest color class based on the values
 int calculateClass(int blue1, int yellow1, int red1);
 
 #endif
