d1917181bea80604a7b9f4d3394dc6b340ebe6c2
hoang-mai05 <hoang.mai@tufts.edu>
2025-12-02 13:51:22 -0500

    Create irDetect.cpp

---
+++ src/.vscode/irDetect.cpp
@@ -0,0 +1,26 @@
+#include "irDetect.h"
+#include "motorcontrol.h"
+#include <Arduino.h>
+
+#define PIN_SENSOR A3
+#define PIN_IR_LED A2
+
+static int ambient;
+
+void irDetectSetup(int delayMs) {
+    pinMode(PIN_IR_LED, OUTPUT);
+    digitalWrite(PIN_IR_LED, LOW);
+    delay(delayMs);
+    ambient = analogRead(PIN_SENSOR);
+    delay(delayMs);
+    digitalWrite(PIN_IR_LED, HIGH);
+}
+
+// returns true if distance is beyond threshold (bot should stop)
+// false otherwise
+bool detectDistance(int threshold) {
+    int current_distance = analogRead(PIN_SENSOR) - ambient;
+    //Serial.println(current_distance);
+    
+    return current_distance < threshold; //distance readings are negative
+}
